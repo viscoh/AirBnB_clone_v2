ae79ddf5f763defbe17622495ea30e65a9550238
Alvinjasper <alvinjspr@gmail.com>
2023-04-18 07:34:53 -0700

    task 3 &  4

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,9 +1,7 @@
--- Prepares a MySQL server for the hbnb project.
+-- script that prepares a MySQL server for the project
+-- script that prepares a MySQL server for the project
 
-CREATE DATABASE IF NOT EXISTS `hbnb_test_db`;
-USE `hbnb_test_db`;
-
-GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost'
-	IDENTIFIED BY 'hbnb_test_pwd';
-GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost'
-	IDENTIFIED BY 'hbnb_test_pwd';
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db . * TO hbnb_test@localhost;
+GRANT SELECT ON performance_schema . * TO hbnb_test@localhost;
